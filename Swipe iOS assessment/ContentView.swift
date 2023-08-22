43989249ec2edda3bf1cf86bac741bc0cef03781
Dishant Nagpal <dishantnagpal10@gmail.com>
2023-08-22 23:50:48 +0530

    Initial Commit

---
+++ Swipe iOS assessment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swipe iOS assessment
+//
+//  Created by Dishant Nagpal on 22/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swipe iOS assessment/Swipe_iOS_assessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swipe_iOS_assessmentApp.swift
+//  Swipe iOS assessment
+//
+//  Created by Dishant Nagpal on 22/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Swipe_iOS_assessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
